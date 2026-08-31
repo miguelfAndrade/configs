00bf1e88bc349f17c4d51796cf4b683fd5b9afbd
miguelfAndrade <miguel.andrade.b@proton.me>
2026-08-31 12:08:33 +0100

    Update oh-my-posh theme setup script

--- theme-setup.sh
+++ theme-setup.sh
@@ -3,6 +3,14 @@
 PROFILE="$HOME/.profile"
 LINE='eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh-themes/tokyonight_storm.omp.json)"'
 
+set -e
+
+curl -fsSL https://ohmyposh.dev/install.sh | bash -s
+oh-my-posh font install meslo
+mkdir ~/config/oh-my-posh-themes/
+cp tokyonight_storm.omp.json  ~/.config/oh-my-posh-theme
+
+
 if ! grep -Fxq "$LINE" "$PROFILE" 2>/dev/null; then
     printf '\n%s\n' "$LINE" >> "$PROFILE"
     echo "Oh My Posh configuration added to $PROFILE"
